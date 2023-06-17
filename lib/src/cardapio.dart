933959445d00c1e5446a70dd5343961e52fd5580
ElitonRoberto <elitongt45@gmail.com>
2023-06-16 21:12:55 -0400

    first commit

--- lib/src/cardapio.dart
+++ lib/src/cardapio.dart
@@ -13,12 +13,25 @@ class PageCardapio extends StatelessWidget {
       body: ListView(
         //cria uma lista
         children: [
-          Container(height: 120, color: Colors.orange),
+          //const Image(image: AssetImage("assets/1.webp")),
+         Container( margin: const EdgeInsets.fromLTRB(1, 1, 1, 2),
+                  child: Column(children: [
+                    Image(
+                      image: AssetImage("assets/1.jpg"),
+                    ),
+                    //color: Colors.yellow,
+                    Text("aaaaa"),
+                  ]),
+          height: 120, color: Colors.orange),
           Container(height: 120, color: Colors.green),
           Container(height: 120, color: Colors.blue),
           Container(height: 120, color: Colors.black),
           Container(height: 120, color: Colors.yellow),
           Container(height: 120, color: Colors.pink),
+          Container(height: 120, color: Colors.orange),
+          Container(height: 120, color: Colors.green),
+          Container(height: 120, color: Colors.blue),
+          Container(height: 120, color: Colors.red),
         ],
       ),
     );
